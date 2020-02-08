ec6b4fc4c94c12112b447032d690bea5c23236d4
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-02-08 22:01:49 +0100

    Feat: Fetching data about city id and weather conditions from AccuWeather API

---
+++ scripts/forecast.js
@@ -0,0 +1,36 @@
+const key = 'K9EOilMHV12q7Avc6mbFwZZwUrrNdcnu';
+
+const getCity = async (city) => {
+  
+  let base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  let query = `?apikey=${key}&q=${city}`
+
+  let response = await fetch(base + query);
+  let data = await response.json();
+
+  return data[0];
+}
+
+const getWeather = async (id) => {
+
+  let base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  let query = `${id}?apikey=${key}`
+
+  let response = await fetch(base + query);
+  let data = await response.json();
+
+  return data[0];
+
+}
+
+getCity('London')
+.then( data => getWeather(data.Key))
+.then( data => console.log(data))
+.catch( err => console.log(err));
+
+
+// getCity('manchester').then(data => {
+//   return getWeather(data.Key);
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => console.log(err));
