0cc1fc2dea9f47b72bddb1b209382cf72e8b3ba1
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-02-08 23:31:55 +0100

    Feat: Updating interface with fetched informations

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 const locationInput = document.querySelector('.change-location');
+const details = document.querySelector('.details');
+const card = document.querySelector('.card');
 
 // Update information about city given from form
 const updateCity = async (city) => {
@@ -6,11 +8,25 @@ const updateCity = async (city) => {
   let cityDetails = await getCity(city);
   let weatherDetails = await getWeather(cityDetails.Key);
 
-  return {
-    cityDetails,
-    weatherDetails
-  }
+  // Returning object
+  return { cityDetails, weatherDetails }
+
 };
+
+const updateUI = (data) => {
+
+  let html = `
+  <h5 class="my-3">${data.cityDetails.LocalizedName}</h5>
+  <div class="my-3">${data.weatherDetails.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${data.weatherDetails.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>
+  `
+  details.innerHTML = html;
+  card.classList.remove('d-none');
+
+}
 
 //Form handler
 locationInput.addEventListener('submit', e => {
@@ -19,7 +35,11 @@ locationInput.addEventListener('submit', e => {
   let city = locationInput.city.value.trim();
 
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err))
   ;
+
+  locationInput.reset();
+
 });
+
