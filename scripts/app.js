880fc049e6338cfdebaa82a64fa1ea8eb8f55e3a
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-02-12 21:19:25 +0100

    JS: creating forecast class in forecast.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,25 +5,13 @@ const card = document.querySelector('.card');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('div.icon');
 
-// Update information about city given from form
-const updateCity = async (city) => {
-
-  let cityDetails = await getCity(city);
-  let weatherDetails = await getWeather(cityDetails.Key);
-
-  // Returning object
-  return { cityDetails, weatherDetails }
-
-};
+const forecast = new Forecast();
 
 // Update User Interface with fetched weather details
 const updateUI = (data) => {
-
   console.log(data);
 
-  // Destructuring
   const { cityDetails, weatherDetails } = data;
-
 
   // Weather details updating
   let html = `
@@ -36,9 +24,7 @@ const updateUI = (data) => {
   `
   details.innerHTML = html;
 
-
   // Time of day updating
-
   weatherDetails.IsDayTime
   ? time.setAttribute('src', 'img/day.svg')
   : time.setAttribute('src', 'img/night.svg');
@@ -49,16 +35,13 @@ const updateUI = (data) => {
   //   time.setAttribute('src', 'img/night.svg');
   // }
 
-
   // Change card visibility after fetching data
   if( card.classList.contains('d-none') ) {
     card.classList.remove('d-none');
   }
 
-
   // Change weather type icon
   icon.innerHTML = `<img src="img/icons/${weatherDetails.WeatherIcon}.svg">`
-
 }
 
 //Form handler
@@ -70,7 +53,7 @@ locationInput.addEventListener('submit', e => {
   // Saving last search to local storage
   localStorage.setItem('city', city);
 
-  updateCity(city)
+  forecast.updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err))
   ;
@@ -81,10 +64,100 @@ locationInput.addEventListener('submit', e => {
 });
 
 // Local storage check to update app with last search value
-
 if (localStorage.getItem('city')) {
-  updateCity(localStorage.getItem('city'))
+  forecast.updateCity(localStorage.getItem('city'))
   .then(data => updateUI(data))
   .catch(err => console.log(err));
 };
+
+
+
+
+
+
+
+// Old version without forecast class
+
+// // Update information about city given from form
+// const updateCity = async (city) => {
+
+//   let cityDetails = await getCity(city);
+//   let weatherDetails = await getWeather(cityDetails.Key);
+
+//   // Returning object
+//   return { cityDetails, weatherDetails }
+
+// };
+
+// // Update User Interface with fetched weather details
+// const updateUI = (data) => {
+
+//   console.log(data);
+
+//   // Destructuring
+//   const { cityDetails, weatherDetails } = data;
+
+
+//   // Weather details updating
+//   let html = `
+//   <h5 class="my-3">${cityDetails.LocalizedName}</h5>
+//   <div class="my-3">${weatherDetails.WeatherText}</div>
+//   <div class="display-4 my-4">
+//     <span>${weatherDetails.Temperature.Metric.Value}</span>
+//     <span>&deg;C</span>
+//   </div>
+//   `
+//   details.innerHTML = html;
+
+
+//   // Time of day updating
+
+//   weatherDetails.IsDayTime
+//   ? time.setAttribute('src', 'img/day.svg')
+//   : time.setAttribute('src', 'img/night.svg');
+
+//   // if (weatherDetails.IsDayTime) {
+//   //   time.setAttribute('src', 'img/day.svg');
+//   // } else {
+//   //   time.setAttribute('src', 'img/night.svg');
+//   // }
+
+
+//   // Change card visibility after fetching data
+//   if( card.classList.contains('d-none') ) {
+//     card.classList.remove('d-none');
+//   }
+
+
+//   // Change weather type icon
+//   icon.innerHTML = `<img src="img/icons/${weatherDetails.WeatherIcon}.svg">`
+
+// }
+
+// //Form handler
+// locationInput.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   let city = locationInput.city.value.trim();
+
+//   // Saving last search to local storage
+//   localStorage.setItem('city', city);
+
+//   updateCity(city)
+//     .then(data => updateUI(data))
+//     .catch(err => console.log(err))
+//   ;
+
+//   // Reset input value
+//   locationInput.reset();
+
+// });
+
+// // Local storage check to update app with last search value
+
+// if (localStorage.getItem('city')) {
+//   updateCity(localStorage.getItem('city'))
+//   .then(data => updateUI(data))
+//   .catch(err => console.log(err));
+// };
 
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,38 +1,68 @@
-const key = 'sOKbRqrPpDEW2hBlEzWGe2BEug70i6GT';
+class Forecast {
 
-// Fetch city informations
-const getCity = async (city) => {
+  constructor() {
+    this.key = 'sOKbRqrPpDEW2hBlEzWGe2BEug70i6GT';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  }
+
+  async updateCity(city) {
+    let cityDetails = await this.getCity(city);
+    let weatherDetails = await this.getWeather(cityDetails.Key);
+    return { cityDetails, weatherDetails };
+  }
+
+  async getCity(city) {
+    let query = `?apikey=${this.key}&q=${city}`
+    let response = await fetch(this.cityURI + query);
+    let data = await response.json();
+    return data[0];
+  }
+
+  async getWeather(id) {
+    let query = `${id}?apikey=${this.key}`
+    let response = await fetch(this.weatherURI + query);
+    let data = await response.json();
+    return data[0];
+  }
+}
+
+
+// const key = 'sOKbRqrPpDEW2hBlEzWGe2BEug70i6GT';
+
+// // Fetch city informations
+// const getCity = async (city) => {
   
-  let base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  let query = `?apikey=${key}&q=${city}`
+//   let base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//   let query = `?apikey=${key}&q=${city}`
 
-  let response = await fetch(base + query);
-  let data = await response.json();
+//   let response = await fetch(base + query);
+//   let data = await response.json();
 
-  return data[0];
-}
+//   return data[0];
+// }
 
-// Fetch weather informations
-const getWeather = async (id) => {
+// // Fetch weather informations
+// const getWeather = async (id) => {
 
-  let base = 'http://dataservice.accuweather.com/currentconditions/v1/'
-  let query = `${id}?apikey=${key}`
+//   let base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+//   let query = `${id}?apikey=${key}`
 
-  let response = await fetch(base + query);
-  let data = await response.json();
+//   let response = await fetch(base + query);
+//   let data = await response.json();
 
-  return data[0];
-}
+//   return data[0];
+// }
 
-// getCity('London')
-//  .then( data => getWeather(data.Key))
-//  .then( data => console.log(data))
-//  .catch( err => console.log(err))
-// ;
+// // getCity('London')
+// //  .then( data => getWeather(data.Key))
+// //  .then( data => console.log(data))
+// //  .catch( err => console.log(err))
+// // ;
 
 
-// getCity('manchester').then(data => {
-//   return getWeather(data.Key);
-// }).then(data => {
-//   console.log(data);
-// }).catch(err => console.log(err));
+// // getCity('manchester').then(data => {
+// //   return getWeather(data.Key);
+// // }).then(data => {
+// //   console.log(data);
+// // }).catch(err => console.log(err));
